2bba6fd45f85e007e390b50d1b27b55170327a0f
Quimby Warden <quimbywarden@gmail.com>
2024-01-18 17:44:51 -0500

    stepping through slowly

--- simulate.py
+++ simulate.py
@@ -1,1 +1,12 @@
 import pybullet as p
+from time import sleep
+
+physicsClient = p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+
+for i in range(1000):
+    p.stepSimulation()
+    sleep(1/200)
+    print(i)
+
+p.disconnect()
