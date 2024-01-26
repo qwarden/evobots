0b566144ece7437673e117ad932901a571bb8d97
Quimby Warden <quimbywarden@gmail.com>
2024-01-26 11:38:56 -0500

    25 towers

--- generate.py
+++ generate.py
@@ -1,5 +1,20 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[1, 1, 1])
+pyrosim.Start_SDF("boxes.sdf")
+
+x = 0
+
+for _ in range(5):
+    y = 0
+    for _ in range(5):
+        z = 0.5
+        size = [1, 1, 1]
+        for _ in range(10):
+            pyrosim.Send_Cube(name="Box", pos=[x, y, z],
+                              size=size)
+            z += 1
+            size = [i * 0.9 for i in size]
+        y += 1
+    x += 1
+
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -1,13 +1,17 @@
 import pybullet as p
+import pybullet_data
 from time import sleep
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-p.loadSDF("box.sdf")
+p.setGravity(0, 0, -9.8)
+planeId = p.loadURDF("plane.urdf")
+p.loadSDF("boxes.sdf")
 
 for i in range(1000):
     p.stepSimulation()
-    sleep(1/200)
+    sleep(1/1000)
     print(i)
 
 p.disconnect()
